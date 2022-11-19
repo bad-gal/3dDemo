8d17a4520701efc828367f596071d76269a77c9a
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-11-19 15:32:22 +0000

    chore: Remove the cube from the scene

--- src/client/client.ts
+++ src/client/client.ts
@@ -9,12 +9,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true } );
-
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 window.addEventListener( 'resize', onWindowResize, false );
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -22,9 +16,7 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     render();
 }
-// TODO: This kinda works socket is polling for messages but fails
-// because there are no connection being made via the server
-let myId = '';
+
 const socket = io();
 socket.on( 'connect', function () {
     console.log('connected');
@@ -39,9 +31,6 @@ socket.on( 'removeClient', ( id: string ) => {
 
 function animate() {
     requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
     render();
 }
