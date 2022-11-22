3acf5f4db8e303ab994202ee09754fa81da9a08b
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-11-22 20:25:52 +0000

    feat: Store the animations in an array and just play the last animation track

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import { io } from 'socket.io-client';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+// holds all the animations
+const allActions: THREE.AnimationAction[] = [];
+
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
 camera.position.set( -5, 3, 10);
 camera.lookAt( new THREE.Vector3( 0, 2, 0 ) );
@@ -35,17 +38,252 @@ scene.add( grid );
 //model
 let mixer: THREE.AnimationMixer;
 const rider = new GLTFLoader();
-rider.load( 'assets/test3.glb', function( gltf ) {
+let clips: THREE.AnimationClip[];
+
+rider.load( 'assets/blue_rider.glb', function( gltf ) {
     scene.add( gltf.scene );
     mixer = new THREE.AnimationMixer( gltf.scene );
-    const clips = gltf.animations;
-    console.log('clips:', clips);
-    const action = mixer.clipAction( clips[15]);
+    clips = gltf.animations;
+
+    let action;
+    const fps = 30;
+    const animationsFrameLocations = [
+        {
+            name: 'jump_down',
+            frameStart: 10,
+            frameEnd: 40,
+        },
+        {
+            name: 'jump_up',
+            frameStart: 40,
+            frameEnd: 70,
+        },
+        {
+            name: 'idle_01',
+            frameStart: 75,
+            frameEnd: 175,
+        },
+        {
+            name: 'idle_02',
+            frameStart: 175,
+            frameEnd: 275,
+        },
+        {
+            name: 'idle_03',
+            frameStart: 275,
+            frameEnd: 375,
+        },
+        {
+            name: 'drive',
+            frameStart: 380,
+            frameEnd: 400,
+        },
+        {
+            name: 'drive_turn_left',
+            frameStart: 405,
+            frameEnd: 425,
+        },
+        {
+            name: 'drive_turn_right',
+            frameStart: 435,
+            frameEnd: 455,
+        },
+        {
+            name: 'drive_fast',
+            frameStart: 460,
+            frameEnd: 480,
+        },
+        {
+            name: 'drive_fast_left',
+            frameStart: 485,
+            frameEnd: 505,
+        },
+        {
+            name: 'drive_fast_right',
+            frameStart: 515,
+            frameEnd: 535,
+        },
+        {
+            name: 'drive_nitro',
+            frameStart: 540,
+            frameEnd: 560,
+        },
+        {
+            name: 'drive_obstacle_01',
+            frameStart: 565,
+            frameEnd: 605,
+        },
+        {
+            name: 'drive_obstacle_02',
+            frameStart: 605,
+            frameEnd: 645,
+        },
+        {
+            name: 'drive_obstacle_03',
+            frameStart: 650,
+            frameEnd: 675,
+        },
+        {
+            name: 'drive_hit_left',
+            frameStart: 675,
+            frameEnd: 695,
+        },
+        {
+            name: 'drive_hit_right',
+            frameStart: 695,
+            frameEnd: 715,
+        },
+        {
+            name: 'drive_kick_left',
+            frameStart: 715,
+            frameEnd: 735,
+        },
+        {
+            name: 'drive_kick_right',
+            frameStart: 735,
+            frameEnd: 755,
+        },
+        {
+            name: 'drive_fail_01',
+            frameStart: 760,
+            frameEnd: 805,
+        },
+        {
+            name: 'drive_fail_02',
+            frameStart: 810,
+            frameEnd: 845,
+        },
+        {
+            name: 'drive_trick_01',
+            frameStart: 850,
+            frameEnd: 900,
+        },
+        {
+            name: 'drive_trick_01_loop',
+            frameStart: 860,
+            frameEnd: 890,
+        },
+        {
+            name: 'drive_trick_02',
+            frameStart: 900,
+            frameEnd: 950,
+        },
+        {
+            name: 'drive_trick_02_loop',
+            frameStart: 910,
+            frameEnd: 940,
+        },
+        {
+            name: 'drive_trick_03',
+            frameStart: 950,
+            frameEnd: 1000,
+        },
+        {
+            name: 'drive_trick_03_loop',
+            frameStart: 960,
+            frameEnd: 990,
+        },
+        {
+            name: 'drive_trick_04',
+            frameStart: 1000,
+            frameEnd: 1050,
+        },
+        {
+            name: 'drive_trick_04_loop',
+            frameStart: 1015,
+            frameEnd: 1035,
+        },
+        {
+            name: 'drive_trick_05',
+            frameStart: 1050,
+            frameEnd: 1100,
+        },
+        {
+            name: 'drive_trick_05_loop',
+            frameStart: 1065,
+            frameEnd: 1085,
+        },
+        {
+            name: 'drive_trick_06',
+            frameStart: 1100,
+            frameEnd: 1150,
+        },
+        {
+            name: 'drive_trick_06_loop',
+            frameStart: 1115,
+            frameEnd: 1135,
+        },
+        {
+            name: 'drive_trick_07',
+            frameStart: 1150,
+            frameEnd: 1200,
+        },
+        {
+            name: 'drive_trick_07_loop',
+            frameStart: 1165,
+            frameEnd: 1185,
+        },
+        {
+            name: 'drive_trick_08',
+            frameStart: 1200,
+            frameEnd: 1250,
+        },
+        {
+            name: 'drive_trick_08_loop',
+            frameStart: 1215,
+            frameEnd: 1235,
+        },
+        {
+            name: 'drive_trick_09',
+            frameStart: 1250,
+            frameEnd: 1300,
+        },
+        {
+            name: 'drive_trick_09_loop',
+            frameStart: 1265,
+            frameEnd: 1285,
+        },
+        {
+            name: 'drive_trick_10',
+            frameStart: 1300,
+            frameEnd: 1360,
+        },
+        {
+            name: 'drive_trick_10_loop',
+            frameStart: 1320,
+            frameEnd: 1340,
+        },
+        {
+            name: 'turn_180',
+            frameStart: 1370,
+            frameEnd: 1400,
+        },
+        {
+            name: 'turn_360',
+            frameStart: 1410,
+            frameEnd: 1450,
+        },
+        {
+            name: 'salto',
+            frameStart: 1460,
+            frameEnd: 1500,
+        },
+    ]
+
+    // store the animations from the model in an array
+    animationsFrameLocations.forEach( function(clip) {
+        let animClip = THREE.AnimationUtils.subclip ( clips[1], clip.name, clip.frameStart, clip.frameEnd, fps );
+        action = mixer.clipAction( animClip );
+        allActions.push( action );
+    });
+
+    if( allActions ){
+        console.log( 'allActions', allActions );
+    }
+
+    // here we just play the last animation in the array
+    action = mixer.clipAction( allActions[43].getClip() );
     action.play();
-    // clips.forEach( function( clip ) {
-    //   const action = mixer.clipAction( clip );
-    //   action.play();
-    // });
 });
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -80,7 +318,6 @@ function animate() {
     if(mixer) {
       mixer.update( clock.getDelta() );
     }
-   
     render();
 }
 
