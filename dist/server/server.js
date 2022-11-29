9e05a96cd14372422699b54b12b12d031b74c26d
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-11-29 22:45:46 +0000

    feat: We update the clients when new players join or leave

--- dist/server/server.js
+++ dist/server/server.js
@@ -32,11 +32,15 @@ const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
 const path_1 = __importDefault(require("path"));
 const process_1 = __importDefault(require("process"));
+// import { io } from 'socket.io-client';
 dotenv.config();
 const port = process_1.default.env.PORT;
+const FPS = 30;
+const SPEED = 0.1;
+const inputsMap = {};
 class App {
     constructor() {
-        this.clients = {};
+        this.clients = [];
         const app = (0, express_1.default)();
         // create virtual paths
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
@@ -44,22 +48,56 @@ class App {
         this.io = new socket_io_1.Server(this.server);
         this.io.on('connection', (socket) => {
             console.log(socket.constructor.name);
-            this.clients[socket.id] = {};
+            // inputsMap[socket.id] = {
+            //   up: false,
+            //   down: false,
+            //   left: false,
+            //   right: false
+            // };
+            this.clients.push({
+                id: socket.id,
+                x: 0,
+                y: 0,
+            });
             console.log(this.clients);
             console.log('connected with socket_id: ', socket.id);
             socket.emit('id', socket.id);
+            // socket.on( 'input', (inputs) => {
+            //   inputsMap[socket.id] = inputs;
+            // });
             socket.on('disconnect', () => {
-                console.log('socket disconnected : ' + socket.id);
-                if (this.clients && this.clients[socket.id]) {
-                    console.log('deleting ' + socket.id);
-                    delete this.clients[socket.id];
+                if (this.clients) {
+                    // get index of client.id matching socket.id
+                    let index = this.clients.findIndex(function (client) {
+                        return client.id === socket.id;
+                    });
+                    // remove the client info
+                    this.clients = [...this.clients.slice(0, index), ...this.clients.slice(index + 1)];
+                    console.log('socket disconnected : ' + socket.id, ' deleting now');
+                    console.log(this.clients); // display the updated list of clients
                     this.io.emit('removeClient', socket.id);
                 }
             });
         });
+        // TODO: Adding and removing of client socket not working properly now that I am using array instead of object
         setInterval(() => {
             this.io.emit('clients', this.clients);
-        }, 50);
+            this.tick();
+        }, 1000 / FPS);
+    }
+    tick() {
+        for (const player of this.clients) {
+            console.log(player, player.id);
+            console.log(player.x, player.y);
+            // const inputs = inputsMap[player.id];
+            // if(inputs.right){
+            //   player.x -= SPEED;
+            // }
+            // if(inputs.left){
+            //   player.x += SPEED;
+            // }
+        }
+        // this.io.emit("players", players);
     }
     Start() {
         this.server.listen(port, function () {
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,14 +4,18 @@ import http from 'http';
 import { Server, Socket } from 'socket.io';
 import path from 'path';
 import process from 'process';
+
 dotenv.config();
 
 const port = process.env.PORT;
+const FPS = 30;
+const SPEED = 0.1;
+const inputsMap = {};
 
 class App {
   private server: http.Server;
   private io: Server;
-  private clients: any = {};
+  private clients: any = [];
 
   constructor() {
     const app = express();
@@ -23,24 +27,67 @@ class App {
 
     this.io.on( 'connection', ( socket: Socket ) => {
       console.log( socket.constructor.name );
-      this.clients[ socket.id ] = {};
+
+      // inputsMap[socket.id] = {
+      //   up: false,
+      //   down: false,
+      //   left: false,
+      //   right: false
+      // };
+
+      this.clients.push({
+        id: socket.id,
+        x: 0,
+        y: 0,
+      });
+
       console.log( this.clients );
       console.log( 'connected with socket_id: ', socket.id );
       socket.emit( 'id', socket.id );
     
+      // socket.on( 'input', (inputs) => {
+      //   inputsMap[socket.id] = inputs;
+      // });
+
       socket.on( 'disconnect', () => {
-        console.log( 'socket disconnected : ' + socket.id );
-        if ( this.clients && this.clients[ socket.id ] ) {
-          console.log('deleting ' + socket.id);
-          delete this.clients[ socket.id ];
+        if( this.clients ) {
+          // get index of client.id matching socket.id
+          let index = this.clients.findIndex(function(client) {
+            return client.id === socket.id;
+          });
+
+          // remove the client info
+          this.clients = [...this.clients.slice(0, index), ...this.clients.slice(index + 1)];
+          console.log( 'socket disconnected : ' + socket.id, ' deleting now' );
+          console.log(this.clients); // display the updated list of clients
+
           this.io.emit( 'removeClient', socket.id );
         }
       });
     });
 
+    // TODO: Adding and removing of client socket not working properly now that I am using array instead of object
     setInterval(() => {
-      this.io.emit( 'clients', this.clients )
-    }, 50);
+      this.io.emit( 'clients', this.clients );
+      this.tick();
+    }, 1000 / FPS );
+  }
+
+  tick() {
+    for(const player of this.clients) {
+      console.log(player, player.id)
+      console.log(player.x, player.y);
+      // const inputs = inputsMap[player.id];
+
+      // if(inputs.right){
+      //   player.x -= SPEED;
+      // }
+      // if(inputs.left){
+      //   player.x += SPEED;
+      // }
+    }
+
+    // this.io.emit("players", players);
   }
 
   public Start() {
